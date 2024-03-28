04e1b79579777c877c7f65fb433041e83484c829
zeka qenberli <ganbarlizaka@gmail.com>
2024-03-28 21:17:33 +0400

    array revers num

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+
+    }
+}
